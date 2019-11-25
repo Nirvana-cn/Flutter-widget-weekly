6787972c1d24f065c71226c786305c2d274b43d6
Nirvana <574014131@qq.com>
2019-11-25 22:51:39 +0800

    expanded

---
+++ week03_expanded/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Expanded'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              flex: 2,
+              child: Container(
+                height: 200,
+                color: Colors.yellow,
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                  height: 200,
+                  color: Colors.green
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
