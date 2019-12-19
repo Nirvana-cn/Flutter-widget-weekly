c806400cc750b55b8d02fa0ac7fe3b83b4ce0595
Nirvana <574014131@qq.com>
2019-12-19 11:44:28 +0800

    StreamBuilder

---
+++ week15_stream_builder/lib/main.dart
@@ -0,0 +1,80 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'StreamBuilder'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  StreamController<int> _controller;
+  int _counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = new StreamController<int>();
+  }
+
+  void _incrementCounter() {
+    _counter++;
+    if(_counter == 21){
+      _controller.close();
+    }
+    _controller.add(_counter);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: StreamBuilder<int>(
+          stream: _controller.stream,
+          builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+            if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                return Text('没有Stream');
+              case ConnectionState.waiting:
+                return Text('等待数据...');
+              case ConnectionState.active:
+                return Text('Active: ${snapshot.data}');
+              case ConnectionState.done:
+                return Text('Stream已关闭');
+            }
+            return null;
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _incrementCounter();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
