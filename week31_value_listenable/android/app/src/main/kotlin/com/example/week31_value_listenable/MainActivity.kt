245338636b8bd2ec3c3b594806cb51d9edbe14d2
wuwei <wei.wu1@mobilityasia.com.cn>
2021-08-30 11:15:29 +0800

    @dev ValueListenableBuilder

---
+++ week31_value_listenable/android/app/src/main/kotlin/com/example/week31_value_listenable/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.week31_value_listenable
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ week31_value_listenable/lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'ValueListenableBuilder'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  ValueNotifier<int> valueNotifier;
+
+
+  @override
+  void initState() {
+    super.initState();
+    valueNotifier = ValueNotifier(0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: ValueListenableBuilder(
+          valueListenable: valueNotifier,
+          builder: (context, value, child) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  '${valueNotifier.value}',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          valueNotifier.value++;
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
