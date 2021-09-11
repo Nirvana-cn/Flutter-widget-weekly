b384e618a43d37254ba5e0a321b8f9f82959731e
wuwei <wei.wu1@mobilityasia.com.cn>
2021-09-11 15:27:05 +0800

    @dev AnimatedIcon

---
+++ week38_animated_icon/android/app/src/main/kotlin/com/example/week38_animated_icon/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.week38_animated_icon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ week38_animated_icon/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'AnimatedIcon'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  AnimationController _progress;
+
+  @override
+  void initState() {
+    super.initState();
+    _progress = new AnimationController(
+        duration: const Duration(milliseconds: 1000),
+        vsync: this
+    );
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          if (_progress.isCompleted) {
+            _progress.reverse();
+          } else {
+            _progress.forward();
+          }
+        },
+        child: AnimatedIcon(
+          icon: AnimatedIcons.play_pause,
+          progress: _progress,
+        ),
+      ),
+    );
+  }
+}
